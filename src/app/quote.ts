71240bcd0f0d6c3b06d8a4b721a4801bdc88c85e
maureenndiema <ndiemam@gmail.com>
2022-04-08 21:30:49 +0300

    add bootstrap

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,14 @@
+import { identifierName } from "@angular/compiler";
+
 export class Quote {
+    id!:number;
+    quote!: string;
+    upvotes!: number;
+    downvotes!: number;
+
+    constructor (id:number, quote:string, upvotes:number, downvotes:number)
+    this.id = id;
+    this.quote = quote;
+    this.upvotes = upvotes;
+    this.downvotes = downvotes;
 }
