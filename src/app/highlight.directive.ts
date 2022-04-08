e712a0123a9399a03ef0cf0f3a49ee7a3d6d177d
maureenndiema <ndiemam@gmail.com>
2022-04-09 01:24:24 +0300

    add background color

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef} from '@angular/core';
 export class HighlightDirective {
 
   constructor(private elem: ElementRef) { 
-    elem.nativeElement.style.backgroundColor = 'purple'
+    elem.nativeElement.style.backgroundColor = 'rgb(36,28,75)'
 
   }
 
