bc186ff00451ce76bceee5abb17536f1b37cdc70
maureenndiema <ndiemam@gmail.com>
2022-04-08 22:10:41 +0300

    add app.component.html changes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,14 +1,15 @@
 import { identifierName } from "@angular/compiler";
 
 export class Quote {
-    id!:number;
-    quote!: string;
-    upvotes!: number;
-    downvotes!: number;
+    id:number;
+    quote: string;
+    upvotes: number;
+    downvotes: number;
 
-    constructor (id:number, quote:string, upvotes:number, downvotes:number)
+    constructor (id:number, quote: string, upvotes: number, downvotes: number){
     this.id = id;
     this.quote = quote;
     this.upvotes = upvotes;
     this.downvotes = downvotes;
+}
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  quotes: Quote[] =[
+    new Quote (1, 'First Quote', 0, 0),
+    new Quote (2, 'Second Quote', 0, 0),
+    new Quote (3, 'Third Quote', 0, 0),
+    new Quote (4, 'Fourth Quote', 0, 0),
+
+  ]
 
   constructor() { }
 
