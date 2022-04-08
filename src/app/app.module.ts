c574dca08caa5cd445c1ff70b35114d5c25262b5
maureenndiema <ndiemam@gmail.com>
2022-04-09 00:01:52 +0300

    add highlight.directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    NavbarComponent
+    NavbarComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,7 +7,7 @@ import { Quote } from '../quote';
 })
 export class QuotesComponent implements OnInit {
 
-  quotes: Quote[] =[
+  public quotes: Quote[] =[
     new Quote (1, 'First Quote', 0, 0),
     new Quote (2, 'Second Quote', 0, 0),
     new Quote (3, 'Third Quote', 0, 0),
