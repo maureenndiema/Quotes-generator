d7c4940a8b0ec5864f91186a19f7c6f1799b6ff7
maureenndiema <ndiemam@gmail.com>
2022-04-09 00:10:38 +0300

    add changes

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,6 +5,9 @@ import { Directive, ElementRef} from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor(private elem: ElementRef) { }
+  constructor(private elem: ElementRef) { 
+    
+  }
+
 
 }
