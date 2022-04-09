41f77dcdd9d7e4a7126f36800d0b473fc639c180
maureenndiema <ndiemam@gmail.com>
2022-04-09 14:48:40 +0300

    add pending changes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,23 +1,27 @@
 // import { identifierName } from "@angular/compiler";
 
 export class Quote {
+  showDescription: boolean;
     id:number;
     quote: string;
     upvotes: number;
     downvotes: number;
+    description : string;
 
-    constructor (id:number, quote: string, upvotes: number, downvotes: number){
+    constructor (id:number, quote: string, upvotes: number, downvotes: number, description:string){
     this.id = id;
     this.quote = quote;
     this.upvotes = upvotes;
     this.downvotes = downvotes;
+    this.description = description;
+    this.showDescription=false;
     
-    export class Quote {
-        showDescription: boolean;
-        constructor(public id: number,public name: string,public description: string){
-          this.showDescription=false;
-        }
-      }
+    // export class Quote {
+    //     showDescription: boolean;
+    //     constructor(public id: number,public name: string,public description: string){
+    //       this.showDescription=false;
+    //     }
+    //   }
         
       }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,15 +8,20 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
 
   public quotes: Quote[] =[
-    new Quote (1, 'First Quote', 1, 0),
-    new Quote (2, 'Second Quote', 0, 0),
-    new Quote (3, 'Third Quote', 0, 0),
-    new Quote (4, 'Fourth Quote', 0, 0),
-    new Quote (5, 'Fifth Quote', 0, 0),
+    new Quote (1, 'First Quote', 1, 0, "description"),
+    new Quote (2, 'Second Quote', 0, 0, "description"),
+    new Quote (3, 'Third Quote', 0, 0,"description"),
+    new Quote (4, 'Fourth Quote', 0, 0,"description"),
+    new Quote (5, 'Fifth Quote', 0, 0, "description"),
 
   ]
   arr: number[] = this.quotes.map(quote=>quote.upvotes)
   highest = Math.max (...this.arr)
+
+  toggleDetails(index: number){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
