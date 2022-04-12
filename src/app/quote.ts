4c9cd92ca1c3092b7aee3c6619f9263b878aa066
maureenndiema <ndiemam@gmail.com>
2022-04-12 12:06:10 +0300

    add pending changes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,28 +2,33 @@
 
 export class Quote {
   showDescription: boolean;
-    id:number;
-    quote: string;
-    author: string;
-    upvotes: number;
-    downvotes: number;
-    description : string;
-    submitted_by: string;
-    postdate: Date;
+  id: number;
+  quote: string;
+  author: string;
+  upvotes: number;
+  downvotes: number;
+  description: string;
+  submitted_by: string;
+  postdate: Date;
 
-    constructor (id:number, quote: string, author: string, upvotes: number, downvotes: number, description:string,submitted_by: string, postdate:Date){
+  constructor(
+    id: number,
+    quote: string,
+    author: string,
+    upvotes: number,
+    downvotes: number,
+    description: string,
+    submitted_by: string,
+    postdate: Date
+  ) {
     this.id = id;
     this.quote = quote;
-    this.author = author
+    this.author = author;
     this.upvotes = upvotes;
     this.downvotes = downvotes;
     this.description = description;
     this.submitted_by = submitted_by;
-    this.postdate =postdate;
-    this.showDescription=false;
-    
-    
-        
-      }
+    this.postdate = postdate;
+    this.showDescription = false;
+  }
 }
-
